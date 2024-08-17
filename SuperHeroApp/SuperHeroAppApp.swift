9891097a17758c5ef41d0c8adbe8aec080115392
Andres Jaimes <andres.jaimes30.aj@gmail.com>
2024-08-16 19:24:25 -0500

    Initial Commit

---
+++ SuperHeroApp/SuperHeroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroAppApp.swift
+//  SuperHeroApp
+//
+//  Created by AB on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
